a900685c048093aa4a7f072cc25e37ab976b93c7
TassoLee <NexusOne.Lee@gmail.com>
2013-05-06 21:48:14 +0800

    Add git alias support

---
+++ share/alias/git.sh
@@ -0,0 +1,9 @@
+[ $(which hub) ] && alias git='hub'
+
+alias gad='git add'
+alias gbr='git branch'
+alias gca='git commit -a'
+alias gci='git commit'
+alias gco='git checkout'
+alias gst='git status'
+
